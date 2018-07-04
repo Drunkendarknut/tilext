b44849eb3bb9c7cc71b8724180e040fbe9a2957c
Håkon Stormo <hstormo@fastmail.com>
2018-07-04 22:14:26 +0200

    Better args handling

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,93 @@ struct Config<'a>
 {
     tile_size: usize,
     output_suffix: &'a str,
+    make_backup: bool,
     input_paths: Vec<PathBuf>
+}
+
+#[derive(Debug, PartialEq)]
+enum ArgsKey
+{
+    TileSize,
+    OutputSuffix,
+    FilePath
+}
+
+fn parse_args<'a>(args: &'a Vec<String>) -> Result<Config<'a>, Box<Error>>
+{
+    let mut tile_size: Option<usize> = None;
+    let mut output_suffix = "";
+    let mut input_paths = Vec::<PathBuf>::new();
+    let mut make_backup = false;
+
+    use ArgsKey::*;
+    let mut current_key = FilePath;
+    for arg in args.as_slice()
+    {
+        match current_key
+        {
+            TileSize =>
+            {
+                tile_size = Some(arg.parse().map_err(
+                    |e| format!("{} (after --tile--size)", e)
+                )?);
+                current_key = FilePath;
+            },
+
+            OutputSuffix =>
+            {
+                output_suffix = &arg;
+                current_key = FilePath;
+            },
+
+            FilePath =>
+            {
+                if arg.starts_with("--")
+                {
+                    current_key = match &arg.chars().as_str()[2..]
+                    {
+                        "tile-size" => TileSize,
+                        "output-suffix" => OutputSuffix,
+                        s => {
+                            println!("Warning: Argument key {} is unknown (ignoring)", s);
+                            FilePath
+                        }
+                    };
+                }
+                else
+                {
+                    input_paths.push(PathBuf::from(&arg));
+                }
+            }
+        }
+    }
+
+    if current_key != FilePath
+    {
+        return Err(format!("Expected another argument (type {:?})", current_key).into());
+    }
+
+    if input_paths.len() == 0
+    {
+        return Err("No file paths specified".into());
+    }
+
+    if output_suffix == ""
+    {
+        make_backup = true;
+        println!("Warning: No output suffix specified. Input file will be overwritten, and a backup (with suffix _backup) will be made. (use --output-suffix if you meant to specify a suffix)");
+    }
+
+    return Ok(Config
+    {
+        tile_size: tile_size.ok_or("No tile size specified (use --tile-size)")?,
+
+        output_suffix,
+
+        make_backup,
+
+        input_paths
+    });
 }
 
 fn get_pixel_index(pixel_x: usize, pixel_y: usize, image_width: usize) -> usize
@@ -21,7 +107,8 @@ fn process_image(config: &Config, path_i: usize) -> Result<(), Box<Error>>
 {
     let input_path = &config.input_paths[path_i];
 
-    println!("\nFile: {:?}:", input_path);
+    println!("File: {:?}:", input_path);
+    println!("  Processing with tile size {}", config.tile_size);
 
     //
     // Read file
@@ -32,18 +119,23 @@ fn process_image(config: &Config, path_i: usize) -> Result<(), Box<Error>>
     println!("  Read {} pixels from file", image.buffer.len());
 
     //
-    // Make backup
+    // Make backup if necessary
     //
 
-    let mut backup_path = input_path.clone();
-    let mut backup_name = backup_path.file_stem().ok_or("Invalid path")?.to_os_string();
-    backup_name.push("_backup");
-    backup_name.push(".png");
-    backup_path.set_file_name(backup_name);
+    if config.make_backup
+    {
+        let mut backup_path = input_path.clone();
+        let mut backup_name = backup_path.file_stem().ok_or("Invalid path")?.to_os_string();
+        backup_name.push("_backup");
+        backup_name.push(".png");
+        backup_path.set_file_name(backup_name);
 
-    lodepng::encode32_file(&backup_path, &image.buffer, image.width, image.height)?;
+        lodepng::encode32_file(&backup_path, &image.buffer, image.width, image.height)?;
 
-    println!("  Wrote {} pixels to {:?}", image.buffer.len(), OsString::from(backup_path));
+        println!("  Wrote {} pixels to {:?}", image.buffer.len(), OsString::from(backup_path));
+    }
+
+    // TODO: Resize image, adding gutters
 
     //
     // Extrude tiles
@@ -135,32 +227,24 @@ fn main()
 {
     println!();
 
-    //
-    // Get arguments
-    //
-
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 4 { panic!("not enough arguments"); }
-
-    let mut input_paths = Vec::<PathBuf>::new();
-    for it in args[3..].iter().map(|s| PathBuf::from(s))
+    let args: Vec<String> = env::args().skip(1).collect();
+    match parse_args(&args)
     {
-        input_paths.push(it);
-    }
+        Ok(config) =>
+        {
+            for i in 0..config.input_paths.len()
+            {
+                if let Err(e) = process_image(&config, i)
+                {
+                    eprintln!("Error: {}", e);
+                }
+            }
+        },
 
-    let config = Config {
-        tile_size: args[1].parse()
-            .expect("The first argument should be the width/height of one tile (exluding gutters). Ex: tilext 16 image.png"),
-        output_suffix: &args[2],
-        input_paths
-    };
-
-    for i in 0..config.input_paths.len()
-    {
-        if let Err(e) = process_image(&config, i)
+        Err(e) =>
         {
             eprintln!("Error: {}", e);
         }
-    }
+    };
+
 }
